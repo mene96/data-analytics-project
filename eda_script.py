31d6d1bba4ac8f4bccac432f021f21339c0b56df
Mounika Bommireddy <mounikabommireddy28@gmail.com>
2025-07-24 11:09:36 -0500

    Updated EDA script with working CSV path and output print

--- eda_script.py
+++ eda_script.py
@@ -1,5 +1,3 @@
-# Simple EDA script
 import pandas as pd
-
-df = pd.read_csv(r"C:\Users\mouni\Downloads\2025-07-20-20-20-59.csv")
+df = pd.read_csv(r"D:\mene628\data-analytics-project\data\2025-07-20-20-20-59.csv")
 print(df.head())
